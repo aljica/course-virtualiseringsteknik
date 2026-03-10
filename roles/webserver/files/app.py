666875e37c248188a2a678ee17aa269d8bc6de9b
Almir Aljic <almir.aljic8991@gmail.com>
2026-03-10 22:31:53 +0100

    Add vars, secrets pipeline, Flask env endpoint

--- roles/webserver/files/app.py
+++ roles/webserver/files/app.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def index():
     return "Hello World from the webserver!"
 
+@app.route("/secret")
+def secret():
+    db_user = os.getenv("DB_USER", "not set")
+    db_password = os.getenv("DB_PASSWORD", "not set")
+    return f"DB_USER: {db_user}, DB_PASSWORD: {db_password}"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
